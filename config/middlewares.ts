b68fb37d98bc14db98655758eba4d413c16ee29a
TrentonFunt <ddejilade22@gmail.com>
2025-11-04 18:05:01 +0100

    Upgrade Strapi to v5.30.0, create sample content, cleanup temp files

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,9 +5,9 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      enabled: true,
+      origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:1338', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174', 'http://127.0.0.1:1338'],
+      credentials: true,
       headers: '*',
-      origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174']
     }
   },
   'strapi::poweredBy',
